779d32e8b4facc3ca307495acee3084e4c08a2b5
Chaitanya Ponduri <chaitanya.ponduri@gmail.com>
2023-04-20 13:34:56 +0530

    Update AppFooter.js

--- layout/AppFooter.js
+++ layout/AppFooter.js
@@ -8,6 +8,7 @@ const AppFooter = () => {
     //logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`
     return (
         <div className="layout-footer">
+            In Association with
             <img src={`${contextPath}/layout/images/tabit.png`} alt="Logo" height="60" className="mr-2" />
             Powered By
             <span className="font-medium ml-2">Pumpkin Tribe</span>
